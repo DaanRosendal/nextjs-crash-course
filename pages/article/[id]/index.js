63476b40dfcc76634501da733b05899ff67a0401
Daan <daanrosendal@outlook.com>
2022-01-20 16:06:51 +0100

    fix key warning and show articles using staticProps/staticPaths

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -11,7 +11,7 @@ const article = ({ article }) => {
   );
 };
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
   );
@@ -22,6 +22,19 @@ export const getServerSideProps = async (context) => {
     props: {
       article,
     },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  const articles = await res.json();
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
   };
 };
 
